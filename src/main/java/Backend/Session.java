1f36e369b21168f47b4ed9219d9fc62fdabdd734
Caminero5 <79474589+Caminero5@users.noreply.github.com>
2021-08-17 17:16:21 -0400

    Update Session.java
    Added comments

--- src/main/java/Backend/Session.java
+++ src/main/java/Backend/Session.java
@@ -10,21 +10,26 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Session {
+    
+    //sends data to server to make a login session
     public String login(String username, String password) throws IOException {
         APIInfo apiInfo = new APIInfo();
 
+       //grabs host URL from the APIInfo class
         String host = apiInfo.getHost();
         String url = host + "/login";
+        //converts strings into a json format for server to use
         String json = "{\"username\": \""+username+"\", \"password\": \""+password+"\"}";
         System.out.println(json);
 
+        //makes connecttion to database
         URL Url = new URL(url);
         HttpURLConnection conn = (HttpURLConnection) Url.openConnection();
         conn.setConnectTimeout(5000);
         conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
         conn.setDoOutput(true);
         conn.setDoInput(true);
-        conn.setRequestMethod("POST");
+        conn.setRequestMethod("POST"); //makes method POST for inserting data
 
         OutputStream os = conn.getOutputStream();
         os.write(json.getBytes("UTF-8"));
@@ -34,20 +39,25 @@ public class Session {
         InputStream in = new BufferedInputStream(conn.getInputStream());
         String results = IOUtils.toString(in, "UTF-8");
 
+        //disconnects everything
         in.close();
         conn.disconnect();
 
         return(results);
     }
 
+    //sends data to server to register
     public String register(String email, String invite_code, String password) throws IOException {
         APIInfo apiInfo = new APIInfo();
 
+        //grabs host from APIInfo class
         String host = apiInfo.getHost();
         String url = host + "/register";
+        //converts string data to json format for server
         String json = "{\"email\": \""+email+"\", \"invite_code\": \""+invite_code+"\", \"password\": \""+password+"\"}";
         System.out.println(json);
 
+        //connects to database
         URL Url = new URL(url);
         HttpURLConnection conn = (HttpURLConnection) Url.openConnection();
         conn.setConnectTimeout(5000);
@@ -64,20 +74,27 @@ public class Session {
         InputStream in = new BufferedInputStream(conn.getInputStream());
         String results = IOUtils.toString(in, "UTF-8");
 
+        //disconnects all
         in.close();
         conn.disconnect();
 
+        //returns the results to user
         return(results);
     }
 
+    
+    //loggs the user out
     public String logout(String sessionID) throws IOException {
         APIInfo apiInfo = new APIInfo();
 
+        //grabs host from APIInfo class
         String host = apiInfo.getHost();
         String url = host + "/logout";
+        //converts session ID string into json format for server
         String json = "{\"sessionID\": \""+sessionID+"\"}";
         System.out.println(json);
 
+        //creates connection
         URL Url = new URL(url);
         HttpURLConnection conn = (HttpURLConnection) Url.openConnection();
         conn.setConnectTimeout(5000);
@@ -94,6 +111,7 @@ public class Session {
         InputStream in = new BufferedInputStream(conn.getInputStream());
         String results = IOUtils.toString(in, "UTF-8");
 
+        //closes connection
         in.close();
         conn.disconnect();
 
