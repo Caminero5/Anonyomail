486612b9ad8607815418af367e2f3a38fb05eef9
Caminero5 <79474589+Caminero5@users.noreply.github.com>
2021-08-17 17:17:01 -0400

    Update APIInfo.java
    comments added

--- src/main/java/Backend/APIInfo.java
+++ src/main/java/Backend/APIInfo.java
@@ -3,6 +3,7 @@ package Backend;
 import java.sql.DriverManager;
 
 public class APIInfo {
+    //creates field defining host so i dont have to switch the host everywhere in the application
     private String host = "http://api.anonyomail.com";
 
 
